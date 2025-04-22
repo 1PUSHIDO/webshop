59b82d9c44f1f6d01243e54fc5fa9c4aac9d5ab2
Your Name <you@example.com>
2025-04-22 11:56:01 +0300

    В ApplicationContext добавлена таблица DbSet и строка подключения

--- Magazine.WebApi/ApplicationContext.cs
+++ Magazine.WebApi/ApplicationContext.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Magazine.Core.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Magazine.WebApi
 {
     public class ApplicationContext : DbContext
     {
+        public DbSet<Product> Products { get; set; }
+
+        private readonly string _connectionString;
+
 
     }
 }
